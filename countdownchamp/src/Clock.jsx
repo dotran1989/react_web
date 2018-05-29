005bbb018250f00d51db84c7b04abee6d50225e2
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-29 17:02:36 +0700

    leading Zero number

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -21,6 +21,13 @@ class Clock extends Component {
         setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
     }
 
+    leading0(num) {
+        if (num < 10) {
+            return '0' + num
+        }
+        return num;
+    }
+
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
         const seconds = Math.floor((time/1000) % 60);
@@ -34,10 +41,10 @@ class Clock extends Component {
     render() {
         return (
             <div>
-                <div className="Clock-days">{this.state.days} days</div>
-                <div className="Clock-hours">{this.state.hours} hours</div>
-                <div className="Clock-minutes">{this.state.minutes} minutes</div>
-                <div className="Clock-seconds">{this.state.seconds} seconds</div>
+                <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+                <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+                <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+                <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
             </div>
         );
     }
