380c05bebc52acba4050abacee3c4b8eaff427be
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-29 14:46:21 +0700

    state of App

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2018'
+        };
+    }
+
     render() {
         return (
             <div className="App">
-                <div className="App-title">Countdown Champ, App Component</div>
+                <div className="App-title">Countdown to {this.state.deadline}</div>
                 <div>
                     <div className="Clock-days">14 days</div>
                     <div className="Clock-hours">30 hours</div>
