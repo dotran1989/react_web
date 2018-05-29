19e59f37345467263d6f1205dfa18a20c7a8da1c
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-29 16:59:04 +0700

    lifecycle method fix error and update clock time per seconds

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -13,21 +13,25 @@ class Clock extends Component {
         console.log('this.props', this.props);
     }
 
+    componentWillMount() { // will be called and updated state before render
+        this.getTimeUntil(this.props.deadline);
+    }
+
+    componentDidMount() { // will be called after rendered
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
+
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
-        console.log('time', time);
         const seconds = Math.floor((time/1000) % 60);
         const minutes = Math.floor((time/1000/60) % 60);
         const hours = Math.floor(time/(1000*60*60) % 24);
         const days = Math.floor(time/(1000*60*60*24));
 
-        console.log('days', days, 'hours', hours, 'minutes', minutes, 'seconds', seconds);
-        this.setState({days: days});
+        this.setState({days, hours, minutes, seconds});
     }
 
     render() {
-        this.getTimeUntil(this.props.deadline);
-
         return (
             <div>
                 <div className="Clock-days">{this.state.days} days</div>
