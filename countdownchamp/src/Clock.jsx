f5f09cfb612708088e8da4edac3c9d8da479aee3
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-29 16:36:47 +0700

    get times to days, hours, minutes and seconds

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -13,7 +13,20 @@ class Clock extends Component {
         console.log('this.props', this.props);
     }
 
+    getTimeUntil(deadline) {
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        console.log('time', time);
+        const seconds = Math.floor((time/1000) % 60);
+        const minutes = Math.floor((time/1000/60) % 60);
+        const hours = Math.floor(time/(1000*60*60) % 24);
+        const days = Math.floor(time/(1000*60*60*24));
+
+        console.log('days', days, 'hours', hours, 'minutes', minutes, 'seconds', seconds);
+    }
+
     render() {
+        this.getTimeUntil(this.props.deadline);
+
         return (
             <div>
                 <div className="Clock-days">{this.state.days} days</div>
@@ -32,3 +45,6 @@ this.props {deadline: "December 25, 2018"}
 deadline: "December 25, 2018"
 __proto__: Object 
 */
+
+// days 209 hours 7 minutes 24 seconds 54
+
