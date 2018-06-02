5f33b069d8b24c1f8d4bd0ae6838f15ad858af84
cuongquoc2710 <cuongdnq@proteusion.com>
2018-06-02 13:51:37 +0700

    set state to query and onKeyPress

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -3,6 +3,17 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state', this.state);
+    }
+
     render() {
         return (
             <div className="App">
@@ -12,8 +23,11 @@ class App extends Component {
                         <FormControl
                             type="text"
                             placeholder="Search for an Artist"
+                            value={this.state.query}
+                            onChange={event => this.setState({query: event.target.value})}
+                            onKeyPress={event => console.log('event.key', event.key)}
                         />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
